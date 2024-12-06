3ee6a5479724fc519b6a34672eb87bd9548f1a46
nguyenduclinh <nguyenduclinh12@gmail.com>
2024-12-06 09:40:46 +0700

    modal & notification toast & header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,50 @@
-'use strict';
+"user strict";
 
 // modal variables
-const modal = document.querySelector('[data-modal]');
-const modalCloseBtn = document.querySelector('[data-modal-close]');
-const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+const modal = document.querySelector("[data-modal]");
+const modalCloseBtn = document.querySelector("[data-modal-close]");
+const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
 
 // modal function
-const modalCloseFunc = function () { modal.classList.add('closed') }
+const modalCloseFunc = function () {
+  modal.classList.add("closed");
+};
 
 // modal eventListener
-modalCloseOverlay.addEventListener('click', modalCloseFunc);
-modalCloseBtn.addEventListener('click', modalCloseFunc);
-
-
-
-
+modalCloseOverlay.addEventListener("click", modalCloseFunc);
+modalCloseBtn.addEventListener("click", modalCloseFunc);
 
 // notification toast variables
-const notificationToast = document.querySelector('[data-toast]');
-const toastCloseBtn = document.querySelector('[data-toast-close]');
+const notificationToast = document.querySelector("[data-toast]");
+const toastCloseBtn = document.querySelector("[data-toast-close]");
 
 // notification toast eventListener
-toastCloseBtn.addEventListener('click', function () {
-  notificationToast.classList.add('closed');
+toastCloseBtn.addEventListener("click", function () {
+  notificationToast.classList.add("closed");
 });
 
-
-
-
-
 // mobile menu variables
-const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
-const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
-const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
-const overlay = document.querySelector('[data-overlay]');
-
+const mobileMenuOpenBtn = document.querySelectorAll(
+  "[data-mobile-menu-open-btn]"
+);
+const mobileMenu = document.querySelectorAll("[data-mobile-menu]");
+const mobileMenuCloseBtn = document.querySelectorAll(
+  "[data-mobile-menu-close-btn]"
+);
+const overlay = document.querySelector("[data-overlay]");
 for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
-
-  // mobile menu function
+  //mobile menu function
   const mobileMenuCloseFunc = function () {
-    mobileMenu[i].classList.remove('active');
-    overlay.classList.remove('active');
-  }
-
-  mobileMenuOpenBtn[i].addEventListener('click', function () {
-    mobileMenu[i].classList.add('active');
-    overlay.classList.add('active');
+    mobileMenu[i].classList.remove("active");
+    overlay.classList.remove("active");
+  };
+  mobileMenuOpenBtn[i].addEventListener("click", function () {
+    mobileMenu[i].classList.add("active");
+    overlay.classList.add("active");
   });
-
-  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
-  overlay.addEventListener('click', mobileMenuCloseFunc);
-
+  mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
+  overlay.addEventListener("click", mobileMenuCloseFunc);
 }
-
-
-
 
 
 // accordion variables
@@ -65,6 +55,7 @@ for (let i = 0; i < accordionBtn.length; i++) {
 
   accordionBtn[i].addEventListener('click', function () {
 
+    
     const clickedBtn = this.nextElementSibling.classList.contains('active');
 
     for (let i = 0; i < accordion.length; i++) {
